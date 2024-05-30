07d8fb289da439c938b7b21eee77ac5b91ccdec2
Mayur-Khandait <anchardo29@gmail.com>
2024-05-30 10:04:51 +0530

    backend code

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+// Connect to DB
+const connectDB = require('./server/config/db');
+
+// Express app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+// Connect to DB
+connectDB();
+
+//for post methods
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static files
 app.use(express.static('public'));
 
 
@@ -14,9 +27,13 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
+// Routes
 app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 
+
+// Server listening
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
