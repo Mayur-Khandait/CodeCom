b2ea660e10af68848033fd5ed2a818228bcd0376
Mayur-Khandait <anchardo29@gmail.com>
2024-05-30 17:05:47 +0530

    updated some admin things

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,14 +1,12 @@
-
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
-// const User = require('../models/User');
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin';
-// const jwtSecret = process.env.JWT_SECRET;
-
+const jwtSecret = process.env.JWT_SECRET;
 
 // /**
 //  * 
@@ -49,34 +47,34 @@ router.get('/admin', async (req, res) => {
 });
 
 
-// /**
-//  * POST /
-//  * Admin - Check Login
-// */
-// router.post('/admin', async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
+/**
+ * POST /
+ * Admin - Check Login
+*/
+router.post('/admin', async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-//     const user = await User.findOne( { username } );
+    const user = await User.findOne({ username });
 
-//     if(!user) {
-//       return res.status(401).json( { message: 'Invalid credentials' } );
-//     }
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
 
-//     const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-//     if(!isPasswordValid) {
-//       return res.status(401).json( { message: 'Invalid credentials' } );
-//     }
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
 
-//     const token = jwt.sign({ userId: user._id}, jwtSecret );
-//     res.cookie('token', token, { httpOnly: true });
-//     res.redirect('/dashboard');
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie('token', token, { httpOnly: true });
+    res.redirect('/dashboard');
 
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 
 // /**
@@ -217,30 +215,31 @@ router.get('/admin', async (req, res) => {
 // // });
 
 
-// /**
-//  * POST /
-//  * Admin - Register
-// */
-// router.post('/register', async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-//     const hashedPassword = await bcrypt.hash(password, 10);
 
-//     try {
-//       const user = await User.create({ username, password:hashedPassword });
-//       res.status(201).json({ message: 'User Created', user });
-//     } catch (error) {
-//       if(error.code === 11000) {
-//         res.status(409).json({ message: 'User already in use'});
-//       }
-//       res.status(500).json({ message: 'Internal server error'})
-//     }
 
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+/**
+ * POST /
+ * Admin - Register
+*/
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: 'User Created', user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: 'User already in use' });
+      }
+      res.status(500).json({ message: 'Internal server error' })
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // /**
 //  * DELETE /
